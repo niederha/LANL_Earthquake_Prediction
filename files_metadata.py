341d2e409131182b1813455d76edab68679c58b1
Loïc Niederhauser <loic.niederhauser@gmail.com>
2019-04-29 13:49:39 +0200

    Commented

--- files_metadata.py
+++ files_metadata.py
@@ -1,3 +1,5 @@
+""" This file contains meta data and informations related to the train and test files from Kaggle"""
+
 EXPECTED_FILE_EXTENSION = '.csv'
 DATA_DIMENSION = 2
 COLUMN_NAME = ['acoustic_data', 'time_to_failure']
